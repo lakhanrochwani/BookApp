c1446668d817cdad42b563ad3a523db5fe805445
lakhanrochwani <rochwanilakhan@gmail.com>
2019-02-12 22:30:29 -0600

    Updates

---
+++ precache-manifest.581f8931faf75fd00a872f8312c75cad.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "0737be9cc433c422fa42",
+    "url": "/BookApp/static/js/runtime~main.0737be9c.js"
+  },
+  {
+    "revision": "e441048646348b88060e",
+    "url": "/BookApp/static/js/main.e4410486.chunk.js"
+  },
+  {
+    "revision": "9c045fed09dc28fe1e1a",
+    "url": "/BookApp/static/js/1.9c045fed.chunk.js"
+  },
+  {
+    "revision": "56d1711412e4d93b36b0352b2c7a7dde",
+    "url": "/BookApp/index.html"
+  }
+];
